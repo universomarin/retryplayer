ef29cc264786bfdfd1652e5ef258093f3fb71da2
Camilo Marin <tiodematias@gmail.com>
2022-11-24 11:45:26 -0500

    Refactor: Add motion to task

--- main.js
+++ main.js
@@ -1,50 +1,23 @@
+let task = null;
+
 function addElement() {
   const newDiv = document.createElement('div');
-  const newContent = document.createTextNode('Nueva tarea');
   newDiv.className = "item";
   newDiv.setAttribute('draggable', 'true');
+  newDiv.setAttribute("id", 'task_' + Math.random().toString(36));
+  const newContent = document.createTextNode('Nueva tarea');
+  newDiv.addEventListener('dragstart', dragStart)
+  newDiv.addEventListener('dragend', dragEnd)
   newDiv.appendChild(newContent);
   const currentDiv = document.getElementById('container');
   const currentToDo = document.getElementById('to-do');
-  const taskNew = currentDiv.insertBefore(newDiv, currentToDo);
-  currentToDo.appendChild(taskNew);
-}
-
-function dragStart() {
-  console.log('drag started');
-  dragItem = this;
-  setTimeout(() => this.className = 'invisible', 0)
-}
-
-function dragEnd() {
-  console.log('drag ended');
-  this.className = 'item'
-  dragItem = null;
-}
-
-function dragDrop() {
-  console.log('drag dropped');
-  this.append(dragItem);
-}
-
-function dragOver(e) {
-  e.preventDefault()
-  console.log('drag over');
-}
-function dragEnter() {
-  console.log('drag entered');
-}
-function dragLeave() {
-  console.log('drag left');
+  task = currentDiv.insertBefore(newDiv, currentToDo);
+  currentToDo.appendChild(task);
+  console.log(task);
 }
 
 const items = document.querySelectorAll('.item')
 const columns = document.querySelectorAll('.column')
-
-items.forEach(item => {
-  item.addEventListener('dragstart', dragStart)
-  item.addEventListener('dragend', dragEnd)
-});
 
 columns.forEach(column => {
   column.addEventListener('dragover', dragOver);
@@ -52,3 +25,29 @@ columns.forEach(column => {
   column.addEventListener('dragleave', dragLeave);
   column.addEventListener('drop', dragDrop);
 });
+
+function dragEnter() {
+  console.log('drag entered', task);
+}
+function dragOver(e) {
+  e.preventDefault()
+  console.log('drag over', task);
+}
+function dragLeave() {
+  console.log('drag left - r', task);
+}
+function dragDrop() {
+  console.log('drag dropped', task);
+  this.append(task);
+  console.log('drag dropped-2', task);
+}
+function dragStart() {
+  console.log('drag started');
+  task = this;
+  setTimeout(() => this.className = 'invisible', 0)
+}
+function dragEnd() {
+  console.log('drag ended');
+  this.className = 'item'
+  task = null;
+}
